ad978df9561f54d854d53094b337296d7ad14b8e
Seth <colts18seth@gmail.com>
2021-01-23 08:43:55 -0500

    fixed db url

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { gotAllBlogs, loginUser, logoutUser, signUpUser, saveBlog, gotBlogDetails, savePost, upVote, downVote, savePostTitle, gotPostDetails, editPost, deletePost, addComment, deleteComment } from './actions';
 
-const BASE_URL = "https://key-blogger-backend.herokuapp.com";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 function getAllBlogs(searchOrFilter) {
     if (searchOrFilter) {
